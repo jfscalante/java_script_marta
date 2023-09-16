1d7b30f69c1bab9b6537b5177f7066d5a06c17b4
jfscalante <scalante2002@gmail.com>
2023-09-15 20:58:31 -0500

    hice el script de numeros

---
+++ numeros.js
@@ -0,0 +1,11 @@
+var respuesta =prompt('ingrese un numero')
+        if( respuesta >= 1){
+            alert ('el numero es positivo');
+        }
+        else if (respuesta < 0){
+            alert ('el numero es negativo');
+        }
+        
+        else {
+            alert ('el numero es cero');
+        }
